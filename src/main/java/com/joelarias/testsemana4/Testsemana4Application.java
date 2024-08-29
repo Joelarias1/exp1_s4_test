840ed7aad5976941fd0fa7a75a78e3ca72a7cc2e
Joel Arias <joelariasparra3@gmail.com>
2024-08-29 10:05:35 -0400

    Inicialización de proyecto

---
+++ src/main/java/com/joelarias/testsemana4/Testsemana4Application.java
@@ -0,0 +1,13 @@
+package com.joelarias.testsemana4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testsemana4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testsemana4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/joelarias/testsemana4/Testsemana4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joelarias.testsemana4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testsemana4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
