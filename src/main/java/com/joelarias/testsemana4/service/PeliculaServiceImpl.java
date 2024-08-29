138a68b64b3a47c9fde14f4c5bf485dc12a24a2b
Joel Arias <joelariasparra3@gmail.com>
2024-08-29 13:09:25 -0400

    Fixes en Service y creación de ServiceImpl

---
+++ src/main/java/com/joelarias/testsemana4/service/PeliculaServiceImpl.java
@@ -0,0 +1,26 @@
+package com.joelarias.testsemana4.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.joelarias.testsemana4.model.Pelicula;
+import com.joelarias.testsemana4.repository.PeliculaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PeliculaServiceImpl implements PeliculaService {
+
+    @Autowired
+    private PeliculaRepository peliculaRepository;
+
+    @Override
+    public List<Pelicula> getAllPeliculas() {
+        return peliculaRepository.findAll();
+    }
+
+    @Override
+    public Optional<Pelicula> getPeliculaById(Long id) {
+        return peliculaRepository.findById(id);
+    }
+}
