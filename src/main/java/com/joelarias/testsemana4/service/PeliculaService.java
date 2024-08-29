173b72c2044d6c0822a854ce1b65ff1f4f3c1fd3
Joel Arias <joelariasparra3@gmail.com>
2024-08-29 12:56:49 -0400

    Creacion Service version 1

--- src/main/java/com/joelarias/testsemana4/service/PeliculaService.java
+++ src/main/java/com/joelarias/testsemana4/service/PeliculaService.java
@@ -1,5 +1,26 @@
 package com.joelarias.testsemana4.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joelarias.testsemana4.model.Pelicula;
+import com.joelarias.testsemana4.repository.PeliculaRepository;
+
+@Service
 public class PeliculaService {
 
+    @Autowired
+    private PeliculaRepository peliculaRepository;
+
+    public List<Pelicula> getAllPeliculas(){
+        return peliculaRepository.findAll();
+    }
+
+    public Optional<Pelicula> getPeliculaById(Long id) {
+        return peliculaRepository.findById(id);
+    }
 }
+
